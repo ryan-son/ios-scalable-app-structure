f65d95899ef4cc7259d73974f70808902ff71468
Geonhee Son <ryan.son1002@gmail.com>
2022-07-30 15:35:46 +0900

    feat: 프로젝트 생성

---
+++ iOSScalableAppStructure/iOSScalableAppStructureApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSScalableAppStructureApp.swift
+//  iOSScalableAppStructure
+//
+//  Created by Geonhee on 2022/07/30.
+//
+
+import SwiftUI
+
+@main
+struct iOSScalableAppStructureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
