02739ee7f88c6ef5b3d11604fd9a85de33e471fc
Geonhee Son <ryan.son1002@gmail.com>
2022-08-01 14:15:36 +0900

    fix: API 상수 중 정의되지 않은 `grantType` 수정

--- iOSScalableAppStructure/Core/Data/API/ApiConstants.swift
+++ iOSScalableAppStructure/Core/Data/API/ApiConstants.swift
@@ -7,7 +7,7 @@
 
 enum ApiConstants {
   static let host = "api.petfinder.com"
-  static let grantType = ""
+  static let grantType = "client_credentials"
   static let clientId = "VKi6JMfSMpZJfJOI5OOjekI8zS9rTAqiAca9KkWth7Qv9KSFYm"
   static let clientSecret = "G5ET7tkG6RiQXPujMzgw74ZM5BizTYZs9RUZf5AG"
 }
