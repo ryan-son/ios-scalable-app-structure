680f2706796d2b40d46e0daefd7c2b785da9948c
Geonhee Son <ryan.son1002@gmail.com>
2022-08-06 18:54:46 +0900

    feat: `AnimalsNearYouVIew` 리스트 infinite scrolling 기능 추가 및 Section 제거

--- iOSScalableAppStructure/AnimalsNearYou/ViewModels/AnimalsNearYouViewModel.swift
+++ iOSScalableAppStructure/AnimalsNearYou/ViewModels/AnimalsNearYouViewModel.swift
@@ -18,9 +18,12 @@ protocol AnimalStore {
 @MainActor
 final class AnimalsNearYouViewModel: ObservableObject {
 
-  @Published var isLoading: Bool
   private let animalFetcher: AnimalsFetcher
   private let animalStore: AnimalStore
+
+  @Published var isLoading: Bool
+  @Published var hasMoreAnimals = true
+  private(set) var page = 1
 
   init(
     isLoading: Bool = true,
@@ -34,7 +37,7 @@ final class AnimalsNearYouViewModel: ObservableObject {
 
   func fetchAnimals() async {
     Task { @MainActor in
-      let animals = await animalFetcher.fetchAnimals(page: 1)
+      let animals = await animalFetcher.fetchAnimals(page: page)
 
       do {
         try await animalStore.save(animals: animals)
@@ -43,6 +46,12 @@ final class AnimalsNearYouViewModel: ObservableObject {
       }
 
       self.isLoading = false
+      self.hasMoreAnimals = animals.isNotEmpty
     }
+  }
+
+  func fetchMoreAnimals() async {
+    page += 1
+    await fetchAnimals()
   }
 }
