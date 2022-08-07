bb33c8a18b9d49a26a7954b5f31e1251ee4003fd
Ryan (Geonhee) Son <69730931+ryan-son@users.noreply.github.com>
2022-08-07 15:51:31 +0900

    `AnimalsNearYouViewModel`의 페이지네이션 테스트를 추가합니다. (#38)
    * test: `AnimalsNearYouViewModel` 페이지네이션 및 마지막 페이지 테스트 추가
    
    * feat: 비동기 구문 실행 중 원치 않는 코드 실행 방지 로직 추가

--- iOSScalableAppStructureTests/AnimalsNearYou/AnimalsNearYouViewModelTestCase.swift
+++ iOSScalableAppStructureTests/AnimalsNearYou/AnimalsNearYouViewModelTestCase.swift
@@ -8,27 +8,70 @@
 import XCTest
 @testable import iOSScalableAppStructure
 
-@MainActor
 final class AnimalsNearYouViewModelTestCase: XCTestCase {
 
   let testContext = PersistenceController.preview.container.viewContext
-  var viewModel: AnimalsNearYouViewModel!
+  var sut: AnimalsNearYouViewModel!
 
-  @MainActor
-  override func setUp() {
-    super.setUp()
-    viewModel = AnimalsNearYouViewModel(
+  override func setUp() async throws {
+    try await super.setUp()
+    sut = AnimalsNearYouViewModel(
       isLoading: true,
       animalFetcher: AnimalsFetcherMock(),
       animalStore: AnimalStoreService(context: testContext)
     )
   }
 
-  func testFetchAnimalsLoadingState() {
-    Task { @MainActor in
-      XCTAssertTrue(viewModel.isLoading, "The view model should be loading, but it isn't")
-      await viewModel.fetchAnimals()
-      XCTAssertFalse(viewModel.isLoading, "The view model shouldn't be loading, but it is")
+  override func tearDown() async throws {
+    sut = nil
+    try await super.tearDown()
+  }
+
+  func testFetchAnimalsLoadingState() async {
+    XCTAssertTrue(sut.isLoading, "The view model should be loading, but it isn't")
+    await sut.fetchAnimals()
+    XCTAssertFalse(sut.isLoading, "The view model shouldn't be loading, but it is")
+  }
+
+  func testUpdatePageOnFetchMoreAnimals() async {
+    XCTAssertEqual(
+      sut.page,
+      1,
+      "the view model's page property should be 1 before fetching, but it's \(sut.page)"
+    )
+    await sut.fetchMoreAnimals()
+    XCTAssertEqual(
+      sut.page,
+      2,
+      "the view model's page property should be 2 after fetching, but it's \(sut.page)"
+    )
+  }
+
+  func testFetchAnimalsEmptyResponse() async {
+    sut = AnimalsNearYouViewModel(
+      isLoading: true,
+      animalFetcher: EmptyResponseAnimalsFetcherMock(),
+      animalStore: AnimalStoreService(context: testContext)
+    )
+
+    let animals = await sut.fetchAnimals()
+
+    if animals.isNotEmpty {
+      XCTAssertFalse(
+        sut.hasMoreAnimals,
+        "hasMoreAnimals should be false with an empty response, but it's true"
+      )
+      XCTAssertFalse(
+        sut.isLoading,
+        "the view model shouldn't be loading after receiving an empty response, but it is"
+      )
     }
+  }
+}
+
+struct EmptyResponseAnimalsFetcherMock: AnimalsFetcher {
+
+  func fetchAnimals(page: Int) async -> [Animal] {
+    return []
   }
 }
