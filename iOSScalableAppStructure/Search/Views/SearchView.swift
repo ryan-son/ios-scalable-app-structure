2e9a8988eb0210f6700a8dd0003287a9db56e586
Geonhee Son <ryan.son1002@gmail.com>
2022-08-07 20:38:50 +0900

    test: `AnimalSearchServiceMock` 추가

--- iOSScalableAppStructure/Search/Views/SearchView.swift
+++ iOSScalableAppStructure/Search/Views/SearchView.swift
@@ -53,6 +53,17 @@ struct SearchView: View {
 
 struct SearchView_Previews: PreviewProvider {
   static var previews: some View {
-    SearchView()
+    SearchView(
+      viewModel: SearchViewModel(
+        animalSearcher: AnimalSearcherMock(),
+        animalStore: AnimalStoreService(
+          context: PersistenceController.preview.container.viewContext
+        )
+      )
+    )
+    .environment(
+      \.managedObjectContext,
+       PersistenceController.preview.container.viewContext
+    )
   }
 }
